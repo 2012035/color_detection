f3a11badc7918f30fbc9d805764eb6e1535d261e
2012035 <114689396+2012035@users.noreply.github.com>
2023-05-10 22:46:28 +0530

    static_color_detections

--- Color_detection/color_detection.py
+++ Color_detection/color_detection.py
@@ -1,1 +1,9 @@
 import cv2 as c
+import numpy as np
+image=c.imread('image/IMG-20220902-WA0014.jpg')
+image=c.resize(image,(300,450))
+c.imshow('Color_Detection',image)
+
+while True:
+    if(c.waitKey(1) & 0xFF == ord('a')):
+        break
